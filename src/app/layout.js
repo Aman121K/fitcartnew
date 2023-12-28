b31913e5ac66aaad13e0c4e39bb8d182de23a3a1
Kuldeep <kuldeepit007@gmail.com>
2023-12-28 12:48:16 +0530

    check name

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ export const metadata = {
   title: 'Fitcart: Trusted Brands, Better Health',
   description: 'Discover top-rated, safe pre-workout &amp; fitness supplements from trusted brands at FitCart. Elevate your fitness journey with our premium products.',
 }
-
 export default function RootLayout({ children }) {
   return (
     <>
